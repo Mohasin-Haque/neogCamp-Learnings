4d774824f2a172a7646af0a8d937b6ad17d734a1
Mohasin-Haque <mohasinhaque23121@gmail.com>
2022-01-11 00:34:17 +0530

    updated codes

---
+++ let,var,const.js
@@ -0,0 +1,43 @@
+// // Tasks:- 
+// // 1. Working with variables
+
+// // 2. Declare two variables: admin and name.
+// // Assign the value "John" to name.
+// // Copy the value from name to admin.
+// // Show the value of admin using alert (must output “John”).
+
+
+// // Giving the right name
+// // importance: 3
+
+
+// // Create a variable with the name of our planet. How would you name such a variable?
+// // Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+// // Solution for the first
+// let admin;
+// let name;
+// name = "john";
+// admin = name;
+// alert(admin);
+
+// // Solution for the second
+
+// var earth;
+// let currentVisitor;
+
+// let a = 3;
+// function earth(){
+//     console.log(a);
+// }
+// earth();
+
+
+var a = 34;
+console.log(a); // 34
+
+// // const a = 34;
+// // console.log(a)
+
+// const a = 10;
+// console.log(a);
---
+++ practice week-1.js
@@ -0,0 +1,7 @@
+//Try converting these codes into ES6 syntax
+var aloo = 1;
+if (aloo == 1) {
+   var a = 2;
+   console.log(a);
+}
+console.log(aloo);
