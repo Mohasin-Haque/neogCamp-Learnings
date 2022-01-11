25409c7661e357748c2a2837a1af59971db372d4
Mohasin-Haque <mohasinhaque23121@gmail.com>
2022-01-11 11:56:34 +0530

    feat: solved question 1

--- practice week-1.js
+++ practice week-1.js
@@ -1,7 +1,17 @@
 //Try converting these codes into ES6 syntax
+//Question
+// var aloo = 1;
+// if (aloo == 1) {
+//    var a = 2;
+//    console.log(a);
+// }
+// console.log(aloo);
+
+
+//Answer
 var aloo = 1;
-if (aloo == 1) {
-   var a = 2;
-   console.log(a);
+if(aloo == true){
+    let a = 2;
+    console.log(a);
 }
 console.log(aloo);
