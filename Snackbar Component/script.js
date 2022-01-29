4ec223ecb08bae25ad63c86c7bbc0254d4f32fb4
Mohasin-Haque <mohasinhaque23121@gmail.com>
2022-01-29 08:49:28 +0530

    feat: added Avatar Component

---
+++ Snackbar Component/script.js
@@ -0,0 +1,24 @@
+// const toastExample = document.querySelector("#toast-example-success");
+// const btnToShowToast = document.querySelector(".show-toast-example-btn");
+// const btnToCloseToast = document.querySelector("#toast-example-success-btn");
+
+// let timerIdForToast;
+// btnToShowToast.addEventListener("click", ()=>{
+//     toastExample.style.display = "block";
+//     timerIdForToast = setTimeout(()=>{
+//         toastExample.style.display = "none";  
+//     },2000)
+// })
+// btnToCloseToast.addEventListener("click", ()=>{
+//     clearTimeout(timerIdForToast);
+//     toastExample.style.display = "none"; 
+// })
+
+
+// <div class="component-display-container">
+//     <div class="toast toast-success" id="st-example-success-btn"></button>
+//     </div>
+//     <button class="btn btn-solid-primary show-toast-examtoast-example-success">
+//         Saved!
+//         <button type="button" class="btn-close" id="toaple-btn">Show Toast</button>
+// </div>
