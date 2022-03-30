ac21f5039778ca4a8d2abf4ca7512adc2bb16342
Mohasin-Haque <mohasinhaque23121@gmail.com>
2022-03-30 22:56:24 +0530

    solved using reducer (normal function)

--- useReducer practice/useReducer.js
+++ useReducer practice/useReducer.js
@@ -1,4 +1,4 @@
-// Take an array and calculate the sum of even and odd numbers using reduce.
+//Que:- Take an array and calculate the sum of even and odd numbers using reduce.
 
 const array = [1, 5, 3, 7, 2];
 
@@ -7,3 +7,21 @@ const output = array.reduce((acc,curr) => {
 }, {even: 0, odd: 0})
 
 console.log(output);
+
+//Que:- Take an array and calculate the sum of even and odd numbers using reduce.
+
+const numList = [1, 3, 55, 22, 44]
+
+function oddAndEvenSumReducer(accumulator, value) {
+  // write your code here
+if(value%2){
+    return {...accumulator, odd: accumulator.odd + value}
+}
+else{
+   return { ...accumulator, even: accumulator.even + value}
+} 
+}
+
+const oddAndEvenSum = numList.reduce(oddAndEvenSumReducer, { even: 0, odd: 0 })
+
+console.log(oddAndEvenSum)
